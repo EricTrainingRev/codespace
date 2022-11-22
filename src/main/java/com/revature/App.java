2bc1e1c9dd08287560678214b727e1905dbd3c32
EricTrainingRev <106328629+EricTrainingRev@users.noreply.github.com>
2022-11-22 23:04:22 +0000

    Proof of concept!

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -1,10 +1,16 @@
 package com.revature;
 
+import java.io.File;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.io.FileHandler;
 /**
  * Hello world!
  *
@@ -13,14 +19,17 @@ public class App
 {
     public static void main( String[] args )
     {
+        WebDriver driver;
+        FirefoxOptions options = new FirefoxOptions();
+        options.addArguments("-headless");
+            driver = new FirefoxDriver(options); 
         try{
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("headless");
-            WebDriver driver = new ChromeDriver(options);
             driver.get("https://www.wikipedia.org/");
             driver.findElement(By.id("searchInput")).sendKeys("puppies");
             driver.findElement(By.className("pure-button-primary-progressive")).click();
-            System.out.println(driver.getTitle());            
+            System.out.println(driver.getTitle());   
+            File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+            FileHandler.copy(scrFile, new File("screenshot.png"));
         } catch (Exception e){
             System.out.println(e.getMessage());
         } finally {
